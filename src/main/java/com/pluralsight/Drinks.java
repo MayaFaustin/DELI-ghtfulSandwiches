6ec92148c180b30b91c3858ae2ccb673e9e86930
Maya Faustin <mayakfaustin@gmail.com>
2024-11-15 04:18:29 -0700

    drinks class should be good

--- src/main/java/com/pluralsight/Drinks.java
+++ src/main/java/com/pluralsight/Drinks.java
@@ -5,24 +5,50 @@ import java.util.Scanner;
 public class Drinks implements ItemPrice {
     static Scanner reader = new Scanner(System.in);
 
-    // attributes
-    // prices 2, 2.50, 3
-    private static final double SMALL_SANDWICH_PRICE = 4.0; private static final double MEDIUM_SANDWICH_PRICE = 6.0; private static final double LARGE_SANDWICH_PRICE = 8.0;
+    private String drinkFlavor;
+    private String drinkSize;
+    private double drinkPrice;
+
+    public Drinks(String drinkSize, double drinkPrice, String drinkFlavor){
+        this.drinkSize = drinkSize;
+        this.drinkPrice = drinkPrice;
+        this.drinkFlavor = drinkFlavor;
+    }
+
+
     public static void addDrinks(){
         System.out.println("Would you like to a drink? (yes/no)");
         String response = reader.nextLine();
+
         if(response.equalsIgnoreCase("yes")){
+            System.out.println("What size drink would you like: small, medium, or large?");
+            String size = reader.nextLine().toLowerCase();
+            double price = 0;
+            switch(size){
+                case "small" -> price = 2.00;
+                case "medium" -> price = 2.50;
+                case "large" -> price = 3.00;
+                default -> System.out.println("That's an incorrect size.Please try again.");
+            }
             System.out.println("Would you like iced tea, lemonade, or soda?");
+            String flavor = reader.nextLine();
+            Drinks drink = new Drinks(size, price, flavor);
+            System.out.println(drink.toString());
 
         } else if (response.equalsIgnoreCase("no")){
-
+            System.out.println("You've selected no drink.");
         } else {
             System.out.println("That's an incorrect selection, please try again.");
         }
+
     }
 
     @Override
+    public String toString() {
+        return "Drink: " + drinkSize + " " + drinkFlavor + ", Price: $" + drinkPrice;
+    }
+    @Override
     public double getPrice() {
-        return 0;
+        return drinkPrice;
     }
 }
