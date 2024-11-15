64190c12498c939e019a8766265bbaf69ec95258
Maya Faustin <mayakfaustin@gmail.com>
2024-11-15 07:25:01 -0700

    sandwich class is basically done, totals are a bit weird, gonna figure that out

--- src/main/java/com/pluralsight/Sandwich.java
+++ src/main/java/com/pluralsight/Sandwich.java
@@ -7,6 +7,9 @@ import java.util.Scanner;
 public class Sandwich {
     static Scanner reader = new Scanner(System.in);
 
+    private static double basePrice;
+    private double extraMeatPrice;
+    private double extraCheesePrice;
     double cheesePrice = 0;
     double meatPrice = 0;
     private double price;
@@ -14,16 +17,17 @@ public class Sandwich {
     private String sandwichSize;
     List<String>toppings;
     private boolean isToasted;
+    private String meat;
+    private String cheese;
+    private String regularToppings;
+    private String sauces;
+    private String sides;
 
     public double getPrice() {
         return price;
     }
     public void setPrice(double price) {
         this.price = price;
-    }
-
-    public void addToppingPrice(double toppingsPrice) {
-        setPrice(getPrice() + toppingsPrice);
     }
 
     public Sandwich(String _breadType, String _sandwichSize,List<String> toppings, boolean _isToasted) {
@@ -34,47 +38,26 @@ public class Sandwich {
     }
 
     public static void addSandwich() {
-        // meat 1, 2, 3 xtra .5, 1, 1.5
-        // cheese .75, 1.5, 2.25 xtra .3, .6, .9
         List<String> myToppings = new ArrayList<>();
-        myToppings.add("steak");
-        myToppings.add("ham");
-        myToppings.add("salami");
-        myToppings.add("roast beef");
-        myToppings.add("chicken");
-        myToppings.add("bacon");
-        myToppings.add("american");
-        myToppings.add("provolone");
-        myToppings.add("cheddar");
-        myToppings.add("swiss");
-        myToppings.add("lettuce");
-        myToppings.add("peppers");
-        myToppings.add("onions");
-        myToppings.add("tomatoes");
-        myToppings.add("jalepeños");
-        myToppings.add("cucumbers");
-        myToppings.add("pickles");
-        myToppings.add("guacamole");
-        myToppings.add("mushrooms");
-        myToppings.add("mayo");
-        myToppings.add("mustard");
-        myToppings.add("ketchup");
-        myToppings.add("ranch");
-        myToppings.add("vinaigrette");
-        myToppings.add("thousand islands");
-        myToppings.add("au jus");
-        myToppings.add("sauce");
 
-        Sandwich sandwich = new Sandwich("", "", myToppings, true);
+        // temporary object
+        Sandwich sandwich = new Sandwich("", "", new ArrayList<>(), false);
+
         sandwich.selectBread();
         sandwich.selectSandwichSize();
+        sandwich.selectMeat();
+        sandwich.selectCheese();
+        sandwich.selectRegularToppings();
+        sandwich.selectSauces();
+        sandwich.selectSides();
         sandwich.toasted();
 
-        System.out.println("What vegetables would you like to add: \nlettuce, peppers, onions, tomatoes, jalepeños, cucumbers, pickles, guacamole, or mushrooms");
+        double totalSandwichPrice = basePrice + sandwich.meatPrice + sandwich.extraMeatPrice + sandwich.cheesePrice + sandwich.extraCheesePrice;
+        sandwich.setPrice(totalSandwichPrice);
 
-        System.out.println("What sauces would you like to add: \nmayo, mustard, ketchup, ranch, thousand islands, or vinaigrette ");
+        System.out.println(sandwich.toString());
+        System.out.println("Price: $" + totalSandwichPrice);
 
-        System.out.println("What sides would you like to add: \nau jus or sauce");
 
     }
 
@@ -96,54 +79,46 @@ public class Sandwich {
     public String selectSandwichSize() {
         System.out.println("What size sandwich would you like: 4 inches, 8 inches, or 12 inches?");
         String sandwichSizeSelection = reader.nextLine();
-
+        this.sandwichSize = sandwichSizeSelection;
         switch (sandwichSizeSelection) {
             case "4 inches" -> {
-                this.sandwichSize = "4 inches";
-                setPrice(5.50);
-                return "4 inches";
+                basePrice = 5.50;
             }
             case "8 inches" -> {
-                this.sandwichSize = "8 inches";
-                setPrice(7.00);
-                return "8 inches";
+                basePrice = 7.00;
             }
             case "12 inches" -> {
-                this.sandwichSize = "12 inches";
-                setPrice(8.50);
-                return "12 inches";
+                basePrice = 8.50;
             }
             default -> {
                 System.out.println("That's not a valid selection. Please try again.");
                 return selectSandwichSize();
             }
         }
+        return sandwichSizeSelection;
     }
 
     public String selectMeat(){ // size dependent prices: 1, 2, 3
         System.out.println("What meat would you like to add: \nsteak, ham, salami, roast beef, chicken or bacon");
-        String meat = reader.nextLine().toLowerCase();
+        meat = reader.nextLine().toLowerCase();
         toppings.add(meat);
-        //  double meatPrice;
         if(this.sandwichSize == "4 inches") {
             meatPrice = 1.0;
-            setPrice(getPrice() + meatPrice);
         } else if(this.sandwichSize == "8 inches"){
             meatPrice = 2.0;
-            setPrice(getPrice() + meatPrice);
         } else if(this.sandwichSize == "12 inches"){
             meatPrice = 3.0;
-            setPrice(getPrice() + meatPrice);
         }
+
         System.out.println("Would you like to add extra meat? (yes/no)");
         String extraMeat = reader.nextLine().toLowerCase();
         if (extraMeat.equals("yes")){
             if(this.sandwichSize == "4 inches") {
-                setPrice(getPrice() + 0.5 + meatPrice);
+                extraMeatPrice = 0.75;
             } else if(this.sandwichSize == "8 inches"){
-                setPrice(getPrice() + 1.0 + meatPrice);
+                extraMeatPrice = 1.50;
             } else if(this.sandwichSize == "12 inches"){
-                setPrice(getPrice() + 1.5 + meatPrice);
+                extraMeatPrice = 2.25;
             }
         }
         return extraMeat;
@@ -151,30 +126,49 @@ public class Sandwich {
 
     public String selectCheese(){
         System.out.println("What cheese would you like to add: \namerican, provolone, cheddar, or swiss");
-        String cheese = reader.nextLine().toLowerCase();
+        cheese = reader.nextLine().toLowerCase();
         toppings.add(cheese);
         if(this.sandwichSize == "4 inches") {
             cheesePrice = 0.75;
-            setPrice(getPrice() + cheesePrice);
         } else if(this.sandwichSize == "8 inches"){
             cheesePrice = 1.5;
-            setPrice(getPrice() + cheesePrice);
         } else if(this.sandwichSize == "12 inches"){
             cheesePrice = 2.25;
-            setPrice(getPrice() + cheesePrice);
         }
         System.out.println("Would you like to add extra cheese? (yes/no)");
         String extraCheese = reader.nextLine().toLowerCase();
         if (extraCheese.equals("yes")){
             if(this.sandwichSize == "4 inches") {
-                setPrice(getPrice() + 0.30 + cheesePrice);
+               extraCheesePrice = 0.30;
             } else if(this.sandwichSize == "8 inches"){
-                setPrice(getPrice() + 0.60 + cheesePrice);
+                extraCheesePrice = 0.60;
             } else if(this.sandwichSize == "12 inches"){
-                setPrice(getPrice() + 0.90 + cheesePrice);
+                extraCheesePrice = 0.90;
             }
         }
         return extraCheese;
+    }
+
+    public String selectRegularToppings()
+    {
+        System.out.println("What vegetables would you like to add: \nlettuce, peppers, onions, tomatoes, jalepeños, cucumbers, pickles, guacamole, or mushrooms");
+        regularToppings = reader.nextLine();
+        toppings.add(regularToppings);
+        return regularToppings;
+    }
+
+    public String selectSauces(){
+        System.out.println("What sauces would you like to add: \nmayo, mustard, ketchup, ranch, thousand islands, or vinaigrette ");
+        sauces = reader.nextLine();
+        toppings.add(sauces);
+        return sauces;
+    }
+
+    public String selectSides(){
+        System.out.println("What sides would you like to add: \nau jus or sauce");
+        sides = reader.nextLine();
+        toppings.add(sides);
+        return sides;
     }
 
     public void toasted(){
@@ -188,6 +182,18 @@ public class Sandwich {
             System.out.println("That is not a valid response. Please try again.");
             toasted();
         }
+    }
+
+    public String toString() {
+        return "✮☆★✮☆★✮☆★✮☆★✮☆★✮☆★✮☆★✮☆★✮☆★✮☆★✮☆★✮☆★✮☆★✮☆★✮☆★✮☆★✮☆★✮☆★✮☆★✮☆★✮☆★"
+                + "\nBread: " + breadType +
+                "\nSandwich size: " + sandwichSize +
+                "\nMeat: " + meat +
+                "\nCheese: " + cheese +
+                "\nVegetables: " + regularToppings +
+                "\nSauce: " + sauces +
+                "\nSides: " + sides +
+                "\nToasted: " + isToasted;
     }
 }
 
